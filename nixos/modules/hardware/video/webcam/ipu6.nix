1b5ea999e4d0aadc671c2e04a3b9677dee60060c
betaboon <betaboon@0x80.ninja>
2023-04-08 15:04:47 +0200

    nixos/hardware/ipu6: init

---
+++ nixos/modules/hardware/video/webcam/ipu6.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+
+  cfg = config.hardware.ipu6;
+
+  kernelPackages = config.boot.kernelPackages;
+
+in
+{
+
+  options.hardware.ipu6 = {
+
+    enable = mkEnableOption (lib.mdDoc "ipu6 kernel module");
+
+    platform = mkOption {
+      default = "ipu6";
+      type = types.enum [ "ipu6" "ipu6ep" ];
+      description = lib.mdDoc ''
+        Set platform to enable kernel module for.
+      '';
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+
+    boot.extraModulePackages = [ kernelPackages.ipu6-drivers ];
+
+    hardware.firmware = optional (cfg.platform == "ipu6") pkgs.ipu6-camera-bin
+      ++ optional (cfg.platform == "ipu6ep") pkgs.ipu6ep-camera-bin;
+
+    services.udev.extraRules = ''
+      SUBSYSTEM=="intel-ipu6-psys", MODE="0660", GROUP="video"
+    '';
+
+  };
+
+}
